d78ed7b6b873be6f5bcf13822957a7cfd739897f
PedroSanches06 <pedro.prudenciano@hyundai-brasil.com>
2026-02-03 16:37:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -46,3 +46,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+// logo após as outras rotas
+import adminRoutes from "./routes/admin.js";
+// ...
+app.use("/admin", adminRoutes);
